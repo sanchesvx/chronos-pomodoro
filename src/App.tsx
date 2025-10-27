42abc809841e7ba557231e8d039b57aea99ce61c
Victor Sanches <victor.sm1208@gmail.com>
2025-10-27 02:04:00 -0300

    aula 23

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
