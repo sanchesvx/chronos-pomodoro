da1ac20e3549ee23055b046d92db7a042fbea5fe
Victor Sanches <victor.sm1208@gmail.com>
2025-10-28 20:44:49 -0300

    aula 33

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,27 @@ import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
+import { Heading } from './components/Heading';
 
 export function App() {
+  let numero = 0;
+
+  function handleClick() {
+    const span = document.getElementById('numero');
+
+    if (!span) return;
+
+    numero += 1;
+    span.innerText = numero.toString();
+    console.log(numero, Date.now());
+  }
+
   return (
     <>
+      <Heading>
+        Número: <span id='numero'>{numero}</span>
+      </Heading>
+      <button onClick={handleClick}>Aumenta</button>
       <Container>
         <Logo />
       </Container>
@@ -30,7 +47,7 @@ export function App() {
         <form className='form' action=''>
           <div className='formRow'>
             <DefaultInput
-              labelText='task'
+              labelText={numero.toString()}
               id='meuInput'
               type='text'
               placeholder='Digite algo'
