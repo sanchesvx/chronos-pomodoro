0aa38680332b5b9c3fe0146e4921175f300913a2
Victor Sanches <victor.sm1208@gmail.com>
2025-10-29 23:28:36 -0300

    aula 36

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css';
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href=''>Entenda como funciona a tecnica pomodoro</a>
+      <a href=''>Entenda como funciona a técnica pomodoro</a>
       <a href=''>Chronos Pomodoro &copy; {new Date().getFullYear()}</a>
     </footer>
   );
