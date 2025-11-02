d91623d6ec36d10eecdef57edfa48895e0b26532
Victor Sanches <victor.sm1208@gmail.com>
2025-11-02 20:06:43 -0300

    aula 49

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import { Home } from './pages/home';
-import './styles/theme.css';
-import './styles/global.css';
-import { useState } from 'react';
-import type { TaskStateModel } from './models/TaskStateModel';
 import { TaskContextProvider } from './contexts/TaskContext';
 
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
-export function App() {
-  const [state, setState] = useState(initialState);
+import './styles/theme.css';
+import './styles/global.css';
 
+export function App() {
   return (
     <TaskContextProvider>
       <Home />
--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -2,8 +2,9 @@ import { useTaskContext } from '../../contexts/TaskContext';
 import styles from './styles.module.css';
 
 export function CountDown() {
-  const taskContext = useTaskContext();
-  console.log(taskContext);
+  const { state } = useTaskContext();
 
-  return <div className={styles.container}>00:00</div>;
+  return (
+    <div className={styles.container}>{state.formattedSecondsRemaining}</div>
+  );
 }
