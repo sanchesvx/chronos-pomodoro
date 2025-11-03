c35b71441b35db4358f5fe379eca52f9e313f9f7
Victor Sanches <victor.sm1208@gmail.com>
2025-11-03 00:58:25 -0300

    aula 54

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 
@@ -8,6 +8,11 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
   return (
     <TaskContext.Provider value={{ state, setState }}>
       {children}
