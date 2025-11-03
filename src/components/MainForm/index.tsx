c826453c758d4fde1e829165bd67c991f626dba2
Victor Sanches <victor.sm1208@gmail.com>
2025-11-03 18:21:52 -0300

    aula 60

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -64,6 +64,7 @@ export function MainForm() {
           type='text'
           placeholder='Digite algo'
           ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
 
