2f3b1b1f7471ededf2582cda4cc5328c022a20c0
Victor Sanches <victor.sm1208@gmail.com>
2025-11-04 18:16:22 -0300

    aula 67

---
+++ src/contexts/TaskContext/taskActions.ts
@@ -0,0 +1,36 @@
+// useReducer <- hook do React que recebe um reducer e um estado inicial
+// reducer <- função que recebe o estado atual e uma ação, e retorna o novo estado
+// state <- o estado atual
+// action <- a ação disparada, geralmente é um objeto com type e (opcionalmente) payload
+// type <- o tipo da ação, geralmente uma string (pode ser enum, constante, etc)
+// payload <- os dados extras enviados junto com a action, se necessário para atualizar o estado
+
+import type { TaskModel } from '../../models/TaskModel';
+
+export const TaskActionTypes = {
+  START_TASK: 'START_TASK',
+  INTERRUPT_TASK: 'INTERRUPT_TASK',
+  RESET_STATE: 'RESET_STATE',
+} as const;
+
+// Aqui definimos o tipo baseado nas chaves do objeto
+export type TaskActionType =
+  (typeof TaskActionTypes)[keyof typeof TaskActionTypes];
+
+export type TaskActionsWithPayload =
+  | {
+      type: typeof TaskActionTypes.START_TASK;
+      payload: TaskModel;
+    }
+  | {
+      type: typeof TaskActionTypes.INTERRUPT_TASK;
+      payload: TaskModel;
+    };
+
+export type TaskActionsWithoutPayload = {
+  type: typeof TaskActionTypes.RESET_STATE;
+};
+
+export type TaskActionModel =
+  | TaskActionsWithPayload
+  | TaskActionsWithoutPayload;
---
+++ src/contexts/TaskContext/taskReducer.ts
@@ -0,0 +1,22 @@
+import type { TaskStateModel } from '../../models/TaskStateModel';
+import { TaskActionTypes, type TaskActionModel } from './taskActions';
+
+export function taskReducer(
+  state: TaskStateModel,
+  action: TaskActionModel,
+): TaskStateModel {
+  switch (action.type) {
+    case TaskActionTypes.START_TASK: {
+      return state;
+    }
+    case TaskActionTypes.INTERRUPT_TASK: {
+      return state;
+    }
+    case TaskActionTypes.RESET_STATE: {
+      return state;
+    }
+  }
+
+  // Sempre deve retornar o estado
+  return state;
+}
