672dcba1f30d500fbdd3c72670ee37e2c8a275f0
Victor Sanches <victor.sm1208@gmail.com>
2025-11-05 13:44:00 -0300

    aula 79

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,15 @@
-import { Link } from 'react-router';
 import styles from './styles.module.css';
+import { RouterLink } from '../RouterLink';
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <Link to='/about-pomodoro/'>
+      <RouterLink href='/about-pomodoro/'>
         Entenda como funciona a técnica pomodoro
-      </Link>
-      <Link to='/'>
+      </RouterLink>
+      <RouterLink href='/'>
         Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚
-      </Link>
+      </RouterLink>
     </footer>
   );
 }
