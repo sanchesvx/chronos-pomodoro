4bbc39042a6a9a2335bf460711eabcd0ae3027d1
Victor Sanches <victor.sm1208@gmail.com>
2025-11-04 22:03:28 -0300

    aula 73

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -11,16 +11,23 @@ export const TaskActionTypes = {
   START_TASK: 'START_TASK',
   INTERRUPT_TASK: 'INTERRUPT_TASK',
   RESET_STATE: 'RESET_STATE',
+  COUNT_DOWN: 'COUNT_DOWN',
+  COMPLETE_TASK: 'COMPLETE_TASK',
 } as const;
 
 // Tipo que representa os valores possíveis de TaskActionTypes
 export type TaskActionType =
   (typeof TaskActionTypes)[keyof typeof TaskActionTypes];
 
-export type TaskActionsWithPayload = {
-  type: typeof TaskActionTypes.START_TASK;
-  payload: TaskModel;
-};
+export type TaskActionsWithPayload =
+  | {
+      type: typeof TaskActionTypes.START_TASK;
+      payload: TaskModel;
+    }
+  | {
+      type: typeof TaskActionTypes.COUNT_DOWN;
+      payload: { secondsRemaining: number };
+    };
 
 export type TaskActionsWithoutPayload =
   | {
@@ -28,6 +35,9 @@ export type TaskActionsWithoutPayload =
     }
   | {
       type: typeof TaskActionTypes.INTERRUPT_TASK;
+    }
+  | {
+      type: typeof TaskActionTypes.COMPLETE_TASK;
     };
 
 export type TaskActionModel =
