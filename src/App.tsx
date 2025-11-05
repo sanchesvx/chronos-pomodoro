3f29e2f0ffc2e5f80d65a7e56f77aedaa838a449
Victor Sanches <victor.sm1208@gmail.com>
2025-11-05 01:48:37 -0300

    aula 77

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import './styles/theme.css';
 import './styles/global.css';
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { MessagesContainer } from './components/MessagesContainer';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { AboutPomodoro } from './pages/AboutPomodoro';
+import { NotFound } from './pages/NotFound';
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
+
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </MessagesContainer>
     </TaskContextProvider>
   );
