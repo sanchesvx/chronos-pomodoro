6f84329f93067a07c10bce1dc52b0c21f9129a30
Victor Sanches <victor.sm1208@gmail.com>
2025-11-05 21:29:44 -0300

    aula 84

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -5,6 +5,7 @@ import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
 import styles from './styles.module.css';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { formatDate } from '../../utils/formatDate';
 
 export function History() {
   const { state } = useTaskContext();
@@ -43,7 +44,7 @@ export function History() {
                   <tr key={task.id}>
                     <td>{task.name}</td>
                     <td>{task.duration}min</td>
-                    <td>{new Date(task.startDate).toISOString()}</td>
+                    <td>{formatDate(task.startDate)}</td>
                     <td>{task.interruptDate}</td>
                     <td>{task.type}</td>
                   </tr>
