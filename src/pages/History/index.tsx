2c5a6b8aaa40e314d819448d20fab1bfec908fdc
Victor Sanches <victor.sm1208@gmail.com>
2025-11-06 02:14:43 -0300

    aula 96

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -38,6 +38,10 @@ export function History() {
   }, [state.tasks]);
 
   useEffect(() => {
+    document.title = 'Histórico - Chronos Pomodoro';
+  }, []);
+
+  useEffect(() => {
     if (!confirmClearHistory) return;
 
     setConfirmClearHistory(false);
