346e513716e2a1e2148da6162182c0549226c9eb
Victor Sanches <victor.sm1208@gmail.com>
2025-11-05 21:46:30 -0300

    aula 86

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -41,13 +41,18 @@ export function History() {
 
             <tbody>
               {state.tasks.map(task => {
+                const taskTypeDictionary = {
+                  workTime: 'Foco',
+                  shortBreakTime: 'Descanso curto',
+                  longBreakTime: 'Descanso longo',
+                };
                 return (
                   <tr key={task.id}>
                     <td>{task.name}</td>
                     <td>{task.duration}min</td>
                     <td>{formatDate(task.startDate)}</td>
                     <td>{getTaskStatus(task, state.activeTask)}</td>
-                    <td>{task.type}</td>
+                    <td>{taskTypeDictionary[task.type]}</td>
                   </tr>
                 );
               })}
